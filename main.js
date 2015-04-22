88fb7191903a9a69ebfef97a4521622934c32dc5
Timothy Lee <timmy.ccl@gmail.com>
2015-04-22 17:20:33 +0800

    調整 data 取得方式

--- main.js
+++ main.js
@@ -1,18 +1,35 @@
 $(document).ready(function() {
-  var dt = $('#dt').dataTable({
-    "ajax": window.location.pathname + 'ebooks_dt.json',
-    "pageLength": 50,
-    "lengthMenu": [[50, 100, 500, 1000, -1], [50, 100, 500, 1000, "All"]],
-    "columnDefs": [
-      {
+  var dataset = [];
+  var deferreds = [];
+  var dataPaths = [
+    window.location.pathname + 'ebooks_dt.json'
+  ];
+
+  for (var i in dataPaths) {
+    deferreds.push($.getJSON(dataPaths[i], function(res) {
+      dataset = dataset.concat(res.data);
+    }));
+  }
+
+  $.when.apply(this, deferreds
+  ).then(function() {
+    var dt = $('#dt').dataTable({
+      //"ajax": window.location.pathname + 'ebooks_dt.json',
+      "data": dataset,
+      "pageLength": 50,
+      "lengthMenu": [[50, 100, 500, 1000, -1], [50, 100, 500, 1000, "All"]],
+      "columnDefs": [
+        {
         "targets": [2, 3, 9, 10, 11, 12],
         "visible": false,
         "searchable": false
-    },
-    {
+      },
+      {
         "targets": [8],
         "searchable": false
-    }
-    ]
+      }
+      ]
+    });
   });
+
 });
